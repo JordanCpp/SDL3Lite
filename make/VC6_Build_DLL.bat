7289d46b818328ff505704603bed007ae21e2107
JordanCpp <Jordan631@yandex.ru>
2025-08-02 11:07:04 +0400

    Fix build VC 6.0

--- make/VC6_Build_DLL.bat
+++ make/VC6_Build_DLL.bat
@@ -2,17 +2,16 @@
 call "C:\Program Files\Microsoft Visual Studio\VC98\Bin\VCVARS32.BAT"
 
 CL /O2 -GX /FeSDL3.dll -LD -DSDL3LITE_SHARED ^
-/I..\dependencies                 ^
-/I..\include\                     ^
-/I..\source\                      ^
-..\source\SDL3Lite\OpenGL\*.cpp            ^
-..\source\SDL3Lite\Platforms\Win32\*.cpp   ^
-..\source\SDL3Lite\Renders\OpenGL1\*.cpp   ^
-..\source\SDL3Lite\Renders\Software\*.cpp  ^
-..\source\SDL3Lite\\*.cpp                  ^
-gdi32.lib                         ^
-opengl32.lib                      ^
-winmm.lib                         ^
+/I..\dependencies                   ^
+/I..\include\                       ^
+/I..\src\                           ^
+..\src\SDL3\OpenGL\*.cpp            ^
+..\src\SDL3\Win32\*.cpp             ^
+..\src\SDL3\Renders\OpenGL1\*.cpp   ^
+..\src\SDL3\*.cpp                   ^
+gdi32.lib                           ^
+opengl32.lib                        ^
+winmm.lib                           ^
 user32.lib
 
 del *.obj
